908844c2c946eef1b3f424bae66737aa0fe3f36c
Anastasiya <v.anastasiya26@mail.ru>
2019-06-23 21:04:42 +0700

    задание 13

--- js/util.js
+++ js/util.js
@@ -13,6 +13,18 @@ window.util = (function () {
         }
       }
       return currentIndex === (array.length - 1 || -1) ? array[0] : array[currentIndex + 1];
+    },
+
+    shuffle: function (arr) {
+      var j;
+      var temp;
+      for (var i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+      return arr;
     }
   };
 })();
